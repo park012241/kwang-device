9fdb50a91edef6122a3800d341de8e3c459955a6
Joosung Park <admin@slowmotion.dev>
2019-06-23 16:45:17 +0900

    axios

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import {testLED, trigger} from './pin';
+import axios from 'axios';
+import {servo, testLED, trigger} from './pin';
 
 let isOn = false;
-trigger.addListener('interrupt', () => {
+trigger.addListener('interrupt', async () => {
     isOn = !isOn;
     testLED.digitalWrite(isOn ? 1 : 0);
+    servo.servoWrite(45);
+    servo.servoWrite(0);
+
+    await axios.get<undefined>('https://google.com/');
 });
