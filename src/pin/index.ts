53a9d217fac4cce93f929dc66e3fc45eff23fa73
Joosung Park <admin@slowmotion.dev>
2019-06-26 09:34:26 +0900

    ?

--- src/pin/index.ts
+++ src/pin/index.ts
@@ -1,11 +1,11 @@
 import {Gpio} from 'pigpio';
 
-const trigger = new Gpio(3, {
+const trigger = new Gpio(2, {
     edge: Gpio.FALLING_EDGE,
     mode: Gpio.INPUT,
 });
 
-const servo = new Gpio(2, {
+const servo = new Gpio(3, {
     mode: Gpio.OUTPUT,
 });
 
